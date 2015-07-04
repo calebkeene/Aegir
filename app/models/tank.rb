6b58d350fad9e7e7ba6730b3a21bdc120c98d185
calebkeene <caleb@hashbang.co.nz>
2015-07-04 23:02:45 +1200

    add basic presence validations on tank model

--- app/models/tank.rb
+++ app/models/tank.rb
@@ -1,5 +1,7 @@
 class Tank < ActiveRecord::Base
   has_many :notifications
   validates :name, presence: true, uniqueness: true
+  validates :fish_number, presence: true
+  validates :volume, presence: true
 
 end
