d5363108d6cbaa1cca6ad7a17f524511bb00b18b
calebkeene <caleb@hashbang.co.nz>
2015-07-04 22:38:58 +1200

    add notification mailer files

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,13 @@
+class NotificationMailer < ApplicationMailer
+	add_template_helper(TanksHelper)
+	default from: 'noreply@aegir.co.nz'
+
+	def water_change
+		# email to send when a waterchange is overdue
+	end 
+
+	def alert(alert_type)
+		# email to send when there is an issue (some parameter is critical)
+	end
+
+end
