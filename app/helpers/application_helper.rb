df826535c5a63a01d16c154fb630aa337553bcfe
calebkeene <caleb@hashbang.co.nz>
2015-07-06 08:16:23 +1200

    fix logic showing top user in stats.haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,25 @@
 module ApplicationHelper
+	
+	def top_user(users)
+		top = User.new
+		top.water_changes = 0
+		e = false
+		equals = Array.new
+		users.each do |u|
+			if u.water_changes > top.water_changes
+				top = u
+			elsif u.water_changes == top.water_changes
+				e = true
+				equals[0] = u.name
+				equals[1] = top.name
+			end
+		end
+		if !e
+			"#{top.name} is the top Aquarium enthusiast!"
+		else
+			"#{equals[0]} and #{equals[1]} are both top Aquarium enthusiasts!"
+		end
+	end
 
 end
+
